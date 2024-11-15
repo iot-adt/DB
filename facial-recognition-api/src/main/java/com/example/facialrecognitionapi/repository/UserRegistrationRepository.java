6cdaf817be932d15ad222db6f1fdfedd65063b18
KangJeungHun <128660252+KangJeungHun@users.noreply.github.com>
2024-11-15 15:58:28 +0900

    Delete facial-recognition-api directory

--- facial-recognition-api/src/main/java/com/example/facialrecognitionapi/repository/UserRegistrationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.facialrecognitionapi.repository;
-
-import com.example.facialrecognitionapi.model.UserRegistration;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRegistrationRepository extends JpaRepository<UserRegistration, Long> {
-    UserRegistration findByName(String name);  // 이름으로 사용자 찾기
-}
